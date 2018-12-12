17ab9097bd11dd27bfccb174e9f4177e22ffdc2f
ライヴライブ 開発 <develop@livlib.co.jp>
2018-12-12 09:39:16 +0900

    Commit on Dec/12/2018

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # django_master01
 Django Master
+
+
+Ubuntu 18.04
+
+	sudo apt install libmysqlclient-dev
+
+	pip3 install mysqlclient
--- proj01/home/views.py
+++ proj01/home/views.py
@@ -1,16 +1,19 @@
+from datetime import datetime
+
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    str_out = ""
-    str_out += "*** home *** start ***<p />"
-    str_out += "ホームです。<p />"
-    str_out += str(request.user.id) + "&nbsp;&nbsp;"
-    str_out += request.user.username + "<p />"
-    str_out += "<a href='app01/'>app01</a><p />"
-    str_out += "<a href='app02/'>app02</a><p />"
-    str_out += '<a href="accounts/logout/">Logout</a><p />'
-    str_out += "*** Dec/11/2018 AM 10:36 ***<p />"
-    str_out += "*** home *** end ***<p />"
-    return HttpResponse(str_out)
+    message = ""
+    message += 'home からのメッセージです。<br />'
+    message += str(request.user.id) + '&nbsp;&nbsp;'
+    message += request.user.username + '<p />'
+    dd = {
+        'hour': datetime.now().hour,
+        'minute': datetime.now().minute,
+        'message': message,
+    }
+    return render(request, 'home.html', dd)
+
